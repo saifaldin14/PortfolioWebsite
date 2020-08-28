8586c8a2f1600b596a62f4cf73b63c4050259e52
Saif Al-Din Ali <saifaldindeveloper@gmail.com>
2020-08-28 08:31:02 -0400

    added dependecies

---
+++ .cache/sync-requires.js
@@ -0,0 +1,16 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/saifal-dinali/Desktop/Projects/React Projects/portfolio/PortfolioWebsite/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/saifal-dinali/Desktop/Projects/React Projects/portfolio/PortfolioWebsite/src/pages/404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("/Users/saifal-dinali/Desktop/Projects/React Projects/portfolio/PortfolioWebsite/src/pages/about.js"))),
+  "component---src-pages-blog-js": hot(preferDefault(require("/Users/saifal-dinali/Desktop/Projects/React Projects/portfolio/PortfolioWebsite/src/pages/blog.js"))),
+  "component---src-pages-contact-js": hot(preferDefault(require("/Users/saifal-dinali/Desktop/Projects/React Projects/portfolio/PortfolioWebsite/src/pages/contact.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/saifal-dinali/Desktop/Projects/React Projects/portfolio/PortfolioWebsite/src/pages/index.js"))),
+  "component---src-pages-projects-js": hot(preferDefault(require("/Users/saifal-dinali/Desktop/Projects/React Projects/portfolio/PortfolioWebsite/src/pages/projects.js")))
+}
+
